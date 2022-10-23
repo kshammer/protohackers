576415ab03ebb4c100dbe60ab406ca1cbf24246c
kshammer <kshammerschmidt@gmail.com>
2022-10-22 17:36:16 -0700

    working =)

--- primetime/src/main.rs
+++ primetime/src/main.rs
@@ -1,8 +1,8 @@
+use serde::{Deserialize, Serialize};
 use std::io::{Read, Write};
-use std::net::{Shutdown, TcpListener, TcpStream};
+use std::net::{TcpListener, TcpStream};
 use std::str;
 use std::thread;
-use serde_json::{Result, Value};
 
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:3333").unwrap();
@@ -27,17 +27,40 @@ fn handle_client(mut stream: TcpStream) {
         let mut read = [0; 1028];
         match stream.read(&mut read) {
             Ok(n) => {
-                println!("stream {:?}", read);
                 if n == 0 {
                     break;
                 }
                 let s = match str::from_utf8(&read) {
-                    Ok(v) => v,
-                    Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+                    Ok(v) => v.trim_matches(char::from(0)),
+                    Err(_) => "n",
                 };
-                println!("Converted {}", s); 
-                let v: Value = serde_json::from_str(s).unwrap();
-                println!("serde value {}", v); 
+                println!("{}", s);
+                // break connection if message is invalid
+                if s == "n" {
+                    stream.write(&read[0..n]).unwrap();
+                    break;
+                }
+                let req: Request = match serde_json::from_str(&s) {
+                    Ok(r) => r,
+                    Err(_) => Request {
+                        method: "notreal".to_string(),
+                        number: 0,
+                    },
+                };
+                // break connection if method is not isPrime
+                if req.method != "isPrime" {
+                    stream.write(&read[0..n]).unwrap();
+                    break;
+                }
+
+                let prime = is_prime(req.number);
+                let resp = Response {
+                    method: "isPrime".to_string(),
+                    prime: prime,
+                };
+                let resp_serial = serde_json::to_string(&resp).unwrap();
+                stream.write(resp_serial.as_bytes()).unwrap();
+                
             }
             Err(err) => {
                 panic!("{}", err);
@@ -56,4 +79,16 @@ fn is_prime(num: i64) -> bool {
         }
     }
     true
+}
+
+#[derive(Serialize, Deserialize)]
+struct Request {
+    method: String,
+    number: i64,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Response {
+    method: String,
+    prime: bool,
 }
